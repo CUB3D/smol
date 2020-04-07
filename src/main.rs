1e42a19bdd34e6aa0757cd0f5eb089ba64f25045
CUB3D <callumthom11@gmail.com>
2020-04-07 01:42:05 +0100

    Cargo fix + fmt

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,39 @@
-use actix_web::{App, HttpResponse, HttpServer, Responder, get, head, post, web};
-use serde::Deserialize;
-use rand::Rng;
-use dotenv::dotenv;
-use std::env;
-use diesel::{MysqlConnection, Connection, RunQueryDsl};
 use crate::models::NewLink;
-use diesel::prelude::*;
-use actix_web::middleware::{Logger, Compress};
 use actix_files::Files;
+use actix_web::middleware::{Compress, Logger};
+use actix_web::{get, head, post, web, App, HttpResponse, HttpServer, Responder};
+use diesel::prelude::*;
+use diesel::{Connection, MysqlConnection, RunQueryDsl};
+use dotenv::dotenv;
+use rand::Rng;
+use serde::Deserialize;
+use std::env;
 
 #[macro_use]
 extern crate diesel;
 
-pub mod schema;
 pub mod models;
+pub mod schema;
 
 #[head("/")]
-async fn index_head() -> impl Responder { HttpResponse::Ok() }
+async fn index_head() -> impl Responder {
+    HttpResponse::Ok()
+}
 
 #[get("/")]
 async fn index() -> impl Responder {
     HttpResponse::Ok().body(include_str!("../templates/index.html"))
 }
 
-
 #[derive(Deserialize, Debug)]
 struct ShortenPayload {
-    pub source: String
+    pub source: String,
 }
 
 #[post("/api/shorten")]
 async fn api_shorten(
     conn: web::Data<MysqlConnection>,
-    json: web::Json<ShortenPayload>
+    json: web::Json<ShortenPayload>,
 ) -> impl Responder {
     use schema::links;
 
@@ -43,7 +44,7 @@ async fn api_shorten(
 
     let new_link = NewLink {
         short_code: &short_code,
-        original_link: &json.source
+        original_link: &json.source,
     };
 
     diesel::insert_into(links::table)
@@ -56,14 +57,12 @@ async fn api_shorten(
 }
 
 #[get("/{short}")]
-async fn short(
-    conn: web::Data<MysqlConnection>,
-    path: web::Path<(String, )>
-) -> impl Responder {
-    use schema::links::dsl::*;
+async fn short(conn: web::Data<MysqlConnection>, path: web::Path<(String,)>) -> impl Responder {
     use self::models::Link;
+    use schema::links::dsl::*;
 
-    let short = links.filter(short_code.eq(&path.0))
+    let short = links
+        .filter(short_code.eq(&path.0))
         .first::<Link>(conn.get_ref())
         .expect("Unable to resolve code");
 
@@ -73,8 +72,7 @@ async fn short(
 }
 
 fn get_db_connection() -> MysqlConnection {
-    let database_url = env::var("DATABASE_URL")
-        .expect("DATABASE_URL must be set");
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
 
     MysqlConnection::establish(&database_url)
         .expect(&format!("Error connecting to {}", database_url))
@@ -84,7 +82,7 @@ fn get_db_connection() -> MysqlConnection {
 async fn main() -> std::io::Result<()> {
     dotenv().ok();
 
-    HttpServer::new( move || {
+    HttpServer::new(move || {
         App::new()
             .data(get_db_connection())
             .service(index)
@@ -95,7 +93,8 @@ async fn main() -> std::io::Result<()> {
             .wrap(Logger::default())
             .wrap(Compress::default())
     })
-    .bind("0.0.0.0:8080").expect("Unable to bind to address")
+    .bind("0.0.0.0:8080")
+    .expect("Unable to bind to address")
     .run()
     .await
 }
--- src/models.rs
+++ src/models.rs
@@ -1,20 +1,18 @@
 use diesel::prelude::*;
 
 use super::schema::links;
-use diesel::mysql::types::Datetime;
-use diesel::sql_types::{Text, Timestamp};
 
 #[derive(Queryable)]
 pub struct Link {
     pub id: i32,
     pub original_link: String,
     pub short_code: String,
-    pub created: chrono::NaiveDateTime
+    pub created: chrono::NaiveDateTime,
 }
 
 #[derive(Insertable)]
-#[table_name="links"]
+#[table_name = "links"]
 pub struct NewLink<'a> {
     pub original_link: &'a str,
-    pub short_code: &'a str
+    pub short_code: &'a str,
 }
